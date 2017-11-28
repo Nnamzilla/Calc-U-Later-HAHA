039f2510a41ec74accc3786998af72147510e957
Nnamdi Massally <nqm14a@lib-301-16.local>
2017-11-27 21:41:09 -0600

    Initial Commit

---
+++ Calc U Later! HAHATests/Calc_U_Later__HAHATests.swift
@@ -0,0 +1,36 @@
+//
+//  Calc_U_Later__HAHATests.swift
+//  Calc U Later! HAHATests
+//
+//  Created by Nnamdi Massally on 11/27/17.
+//  Copyright © 2017 Nnamdi Massally. All rights reserved.
+//
+
+import XCTest
+@testable import Calc_U_Later__HAHA
+
+class Calc_U_Later__HAHATests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
