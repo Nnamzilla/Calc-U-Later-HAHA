bd9525443fbefe631f429264be8a91fdce76ad95
Nnamdi Massally <nqm14a@lib-301-16.local>
2017-11-28 22:09:10 -0600

    Almost got basic calc functions working

--- Calc U Later! HAHA/ViewController.swift
+++ Calc U Later! HAHA/ViewController.swift
@@ -10,12 +10,63 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var numOnScreen:Double = 0;
+    var prevNum:Double = 0;
+    var executeFunc = false
+    
     @IBOutlet weak var numberDisplay: UILabel!
     
     @IBAction func numbers(_ sender: UIButton)
     {
-        numberDisplay.text = numberDisplay.text! + String(sender.tag - 1)
+        if executeFunc == true
+        {
+            numberDisplay.text = String(sender.tag - 1)
+            numOnScreen = Double(numberDisplay.text!)!
+            executeFunc = false
+        }
+        else
+        {
+            numberDisplay.text = numberDisplay.text! + String(sender.tag - 1)
+            numOnScreen = Double(numberDisplay.text!)!
+        }
     }
+    
+
+    @IBAction func operations(_ sender: UIButton)
+    {
+        if numberDisplay.text != "" && sender.tag != 11 && sender.tag != 14
+        {
+            
+            prevNum = Double(numberDisplay.text!)!
+            
+            if sender.tag == 18 // Addition
+            {
+                numberDisplay.text = "+"
+            }
+            else if sender.tag == 17 // Subtraction
+            {
+                
+            }
+            else if sender.tag == 16 // Multiplication
+            {
+                
+            }
+            else if sender.tag == 15 // Division
+            {
+                
+            }
+            else if sender.tag == 13 // Percentage
+            {
+                
+            }
+            
+            executeFunc = true;
+            
+        }
+        
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
